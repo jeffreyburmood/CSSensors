a75d9e4d2dc0efc4fbc73ad878964a037fc46469
Jeffrey Burmood <mountainlifeissimple@icloud.com>
2025-11-03 08:26:06 -0700

    Added code for weather API

---
+++ ExternalSensors/Weather/settings.py
@@ -0,0 +1,10 @@
+""" This file loads the required API keys from the local environment. """
+
+import os
+
+try:
+    AMBIENT_ENDPOINT = "https://rt.ambientweather.net/v1"
+    AMBIENT_APPLICATION_KEY = os.environ["AMBIENT_APPLICATION_KEY"]
+    AMBIENT_API_KEY = os.environ["AMBIENT_API_KEY"]
+except KeyError:
+    pass
