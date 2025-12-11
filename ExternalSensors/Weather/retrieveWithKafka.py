4ab2cfeb5907d3816adf826ca91a26dc21e0525d
Jeffrey Burmood <mountainlifeissimple@icloud.com>
2025-12-10 17:04:46 -0700

    process stop command
    can now start and stop web socket processing

--- ExternalSensors/Weather/retrieveWithKafka.py
+++ ExternalSensors/Weather/retrieveWithKafka.py
@@ -9,12 +9,12 @@ KAFKA_BOOTSTRAP_SERVERS = 'localhost:9092'
 KAFKA_COMMAND_TOPIC = 'weather-command'
 KAFKA_GROUP_ID = 'weather-group'
 
-async def process_websocket(start_event, termination_event):
+async def process_websocket(start_event, stop_event, termination_event):
     while not termination_event.is_set():
         await start_event.wait()
         try:
             async with AsyncManagedWebsocketResource('weather') as websocket:
-                while not termination_event.is_set():
+                while not termination_event.is_set() and not stop_event.is_set():
                     try:
                         await asyncio.sleep(2) # handling the data events via the websocket performed in the resource object
                     except Exception as e:
@@ -27,7 +27,7 @@ async def process_websocket(start_event, termination_event):
             await asyncio.sleep(2)
         await asyncio.sleep(1)
 
-async def process_kafka(start_event, termination_event):
+async def process_kafka(start_event, stop_event, termination_event):
     consumer = AIOKafkaConsumer(
         KAFKA_COMMAND_TOPIC,
         bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
@@ -38,27 +38,34 @@ async def process_kafka(start_event, termination_event):
     )
     await consumer.start()
     try:
-        async for msg in consumer:
-            print(
-                "{}:{:d}:{:d}: key={} value={} timestamp_ms={}".format(
+        while not termination_event.is_set():
+
+            async for msg in consumer:
+                print(
+                    "{}:{:d}:{:d}: key={} value={} timestamp_ms={}".format(
                     msg.topic, msg.partition, msg.offset, msg.key, msg.value,
                     msg.timestamp))
-            command = msg.value
-            print(f'Command received by consumer = {command}')
-            should_terminate = await handle_command(command, start_event, termination_event)
-            if should_terminate:
-                break
+                command = msg.value
+                print(f'Command received by consumer = {command}')
+                should_terminate = await handle_command(command, start_event, stop_event, termination_event)
+                if should_terminate:
+                    break
     finally:
         await consumer.stop()
-        exit(1)
 
-async def handle_command(command, start_event, termination_event):
+async def handle_command(command, start_event, stop_event, termination_event):
     print(f"Received command: {command}")
     # action = command.get("action")
     if command == "start":
         if not start_event.is_set():
             print("Start command received. Starting WebSocket processing.")
             start_event.set()
+            stop_event.clear()
+    elif command == "stop":
+        if not stop_event.is_set():
+            print("Stop command event received, shutting down websocket connection")
+            stop_event.set()
+            start_event.clear()
     elif command == "terminate":
         print("Terminate command received. Shutting down application.")
         termination_event.set()
@@ -67,10 +74,11 @@ async def handle_command(command, start_event, termination_event):
 
 async def main() -> None:
     start_event = asyncio.Event()
+    stop_event = asyncio.Event()
     termination_event = asyncio.Event()
     await asyncio.gather(
-        process_websocket(start_event, termination_event),
-        process_kafka(start_event, termination_event)
+        process_websocket(start_event, stop_event, termination_event),
+        process_kafka(start_event, stop_event, termination_event)
     )
 
 if __name__ == "__main__":
--- ExternalSensors/Weather/weatherWebsocketResource.py
+++ ExternalSensors/Weather/weatherWebsocketResource.py
@@ -1,10 +1,39 @@
 """ This file contains the class and associated methods for managing the weather data websocket in a Context Manager """
 import asyncio
 import os
+from datetime import datetime, timezone
 from typing import Any
+from zoneinfo import ZoneInfo
 
 from aioambient import Websocket
 from dotenv import load_dotenv
+
+def convert_utc_to_timezone(utc_date: str, tz: str) -> str:
+    """
+    Converts a python datetime string in UTC to the target IANA time zone.
+    :param utc_date: str, e.g. "2025-11-12 14:06:00+00:00"
+    :param tz: IANA time zone string, e.g. "America/New_York"
+    :return: String of converted datetime in same format
+    """
+    # Parse the datetime string as UTC and convert to a datetime object
+    dt = datetime.strptime(utc_date, "%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=timezone.utc)
+    # convert datetime object to target timezone
+    dt_target = dt.astimezone(ZoneInfo(tz))
+
+    return dt_target.strftime("%Y-%m-%d %H:%M:%S")
+
+def process_weather_data(current_data):
+    """
+    Takes the current real time weather data and extracts the relevant data and stores it in the database
+    :param current_data: Dictionary of retrieved weather station data
+    """
+    mac_addr = os.getenv('CABIN_MAC')
+
+    if current_data['macAddress'] == mac_addr:
+        local_datetime = convert_utc_to_timezone(current_data['date'], current_data['tz'])
+        print(f'local date time = {local_datetime}')
+    else:
+        pass
 
 # Define a method that should be fired when the websocket client
 # connects:
@@ -23,12 +52,14 @@ async def data_coroutine(data):
     """Wait for 3 seconds, then print the data received."""
     await asyncio.sleep(3)
     print(f"Data received async: {data}")
+    process_weather_data(data)
 
 # Define a method that should be run when the websocket client
 # disconnects:
-def disconnect_method(data):
-    """Print a simple "goodbye" message."""
-    print("Client has disconnected from the websocket")
+async def disconnect_coroutine(data):
+    """Wait for 3 seconds, then print a simple "goodbye" message."""
+    await asyncio.sleep(3)
+    print("Client has disconnected from the websocket async")
 
 
 def configure_websocket() -> Websocket:
@@ -36,13 +67,12 @@ def configure_websocket() -> Websocket:
     load_dotenv()
     API_KEY = os.getenv('AMBIENT_API_KEY')
     APP_KEY = os.getenv('AMBIENT_APPLICATION_KEY')
-    mac_addr = os.getenv('CABIN_MAC')
 
     websocket = Websocket(APP_KEY, API_KEY)
     websocket.on_connect(connect_method)
     websocket.on_subscribed(subscribed_method)
     websocket.async_on_data(data_coroutine)
-    websocket.on_disconnect(disconnect_method)
+    websocket.async_on_disconnect(disconnect_coroutine)
 
     return websocket
 
